7419f37cba5cb1f49aab62ec8aa692e2da03f63d
Adam Nelip <Adam.Nelip@gmail.com>
2018-08-01 20:43:59 +0200

    working app

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function (req, res) {
+    res.render('main')
+});
+
+app.get('/auth/google', function (req, res) {
+    res.render('Logged', {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    });
+});
+
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
